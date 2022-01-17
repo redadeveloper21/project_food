cbbcde183b5b28d07969ea8c469fd52e0c72fd33
reda <96940975+redadeveloper21@users.noreply.github.com>
2022-01-17 23:31:31 +0300

    jjjjj

--- lib/Screen/food_screen.dart
+++ lib/Screen/food_screen.dart
@@ -44,22 +44,8 @@ class _FoodScreenState extends State<FoodScreen> {
               fit: BoxFit.cover,
             ),
             // Container(
-            //   height: 42,
-            //   margin: const EdgeInsets.fromLTRB(16, 16, 16, 16),
-            //   decoration: BoxDecoration(
-            //     borderRadius: BorderRadius.circular(12),
-            //     color: Colors.white,
-            //     border: Border.all(color: Colors.black26),
-            //   ),
-            //   padding: const EdgeInsets.symmetric(horizontal: 8),
-            //   child: TextField(
-            //     controller: null,
-            //     decoration: InputDecoration(
-            //       icon: Icon(Icons.search, color: Colors.grey),
-            //       border: InputBorder.none,
-            //     ),
-            //   ),
-            // ),
+            // /: 42,
+            // ,
             Container(
               height: MediaQuery.of(context).size.height * 0.8,
               child: ListView.builder(
